c83ce417e2dcc71d222eb7009e77860935887cf4
shashirathee <154332412+shashirathee@users.noreply.github.com>
2024-04-02 12:35:32 +0000

    02

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,9 @@
+const accountID = 14453
+let accounntEmail ="shashi@gmail.com"
+var accountPassword = "12345"
+accountCity = "Rohtak"
+let hihi
+
+accounntEmail= "hihi@gmail.com"
+// console.log(accountID, accounntEmail);
+console.table([accountID,accounntEmail, accountPassword, accountCity, hihi]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict";
+
+// alert("Hello")
+// alert(3+3) // we are using nodejs, not browsser
+// console.log(3+3)
+
+console.log("shashi");
+
+
+let name = "shashi"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => "'
+//boolean => true/false
+//null=> standalone value
+// undefined =>
+// symbol => unique
+
+console.log(typeof age);
