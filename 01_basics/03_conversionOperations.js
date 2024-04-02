f7c06aefb7379e8c65d18fb25b6cd7d09ba6fb46
shashirathee <154332412+shashirathee@users.noreply.github.com>
2024-04-02 17:53:39 +0000

    data types

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,5 +1,48 @@
-let score = 33
+let score = "33asd"
 
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
+
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "23" => 23
+// "23asd" => NaN
+
+let isLoggedIn ="hihi"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+
+
+let soneNumber=33
+let stringNumber = String(soneNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+
+// ---------------------------------------------------------------
+
+let value =2
+
+let negValue = -value
+console.log(negValue);
+
+
+let str1="hello"
+let str2="shashi"
+let str3 = str1 + str2
+console.log(str3);
+
+
+
+console.log(2+2);
+console.log(+true);
+
+console.log("2"==="2");
---
+++ 01_basics/datatype-summary.js
@@ -0,0 +1,16 @@
+// Primitive
+
+// 7types: String, Number, Boolean, Null, Undefined, Symbol, BigInt
+
+
+
+// Reference (Non primitive)
+
+// array, objects, functions
+
+const id = Symbol('123')
+const anotherid = Symbol('123')
+
+console.log(id);
+console.log(anotherid);
+console.log(id===anotherid);
