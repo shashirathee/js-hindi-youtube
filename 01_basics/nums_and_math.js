6306d467e4429b2842e4616a4da8d529583ec8bf
shashirathee <154332412+shashirathee@users.noreply.github.com>
2024-04-19 10:41:04 +0000

    math or num

---
+++ 01_basics/nums_and_math.js
@@ -0,0 +1,38 @@
+const score = 400 
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+const otherNumber = 123.8977
+
+// console.log(otherNumber.toPrecision(3));
+
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'))
+
+
+
+// +++++++++++++MATH+++++++++++++++++++++
+
+console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.0000001));
+// console.log(Math.floor(4.1)); 
+// console.log(Math.min(4,3,2,1,5,6,7,7));
+// console.log(Math.max(4,3,2,1,5,6,7,7));
+
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+const min =10
+const max =20
+
+console.log(Math.floor(Math.random() * (max-min +1))+ min)
+// important
